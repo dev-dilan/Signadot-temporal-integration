ae92a8ed05557f35aa5bac7f803fd0c20d9d2a6d
dilan <dev.dilan@gmail.com>
2025-06-27 09:49:11 +0000

    remove docker compose

--- down.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker compose -f docker-compose/docker-compose.yml down
-docker compose down
-docker rmi -f $(docker images -q 'temporal*')
-docker rmi -f $(docker images -q 'temporalio/*')
